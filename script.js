9ec20293e8dca0753be5d95d2d36c604bd9072b9
bellae6-cyber <bellae6@miamioh.edu>
2026-03-21 17:43:42 -0400

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function showTip() {
-  const tips = [
-    "Use heat protectant before styling.",
-    "Don't wash your hair every day.",
-    "Trim your ends regularly.",
-    "Use a silk pillowcase to reduce breakage.",
-    "Deep condition once a week."
+    const tips = [
+        "Use heat protectant before styling.",
+        "Don't wash your hair every day.",
+        "Trim your ends regularly.",
+        "Use a silk pillowcase to reduce breakage.",
+        "Deep condition once a week."
     ];
 
-const randonTip = tips[Math.floor(Math.random() * tips.length)];
+    const randomTip = tips[Math.floor(Math.random() * tips.length)];
 
-document.getElementById("tipText").innerText = randomTip;
+    document.getElementById("tipText").innerText = randomTip;
+}
